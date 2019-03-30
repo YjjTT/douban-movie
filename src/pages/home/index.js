1fa9f6b1e6640b12647e16b9eec404d7c5e1f0e6
liyang <xxbarca@163.com>
2019-03-31 01:03:46 +0800

    正在热映 数据

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import Slider from 'react-slick'
 import { Rate, Button } from 'antd'
+import axios from 'axios'
 import './index.scss'
 class Home extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            dataList: []
+        }
+    }
+
+    // 正在热映电影
+    loadTheaterData = () => {
+        axios({
+            method: 'get',
+            url: '/v2/movie/in_theaters'
+        }).then(res => {
+            if (res.status === 200) {
+                
+                this.setState({
+                    dataList: res.data.subjects
+                })
+            }
+        })
+
+    }
+
+    componentDidMount() {
+        this.loadTheaterData()
+    }
+
     render() {
         const settings = {
             dots: true,
@@ -21,7 +50,9 @@ class Home extends React.Component {
                                 <a className='in-theater' href='/'>全部正在热映>></a>
                                 <a className='will-in-theater' href='/'>即将上映>></a>
                             </div>
-                            <ul className='clearfix'>
+                            <Slider {...settings}>
+                                <div>
+                                <ul className='clearfix'>
                                 
                                 <li className='each'>
                                     <a href='/'>
@@ -74,6 +105,63 @@ class Home extends React.Component {
                                     <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
                                 </li>
                             </ul>
+                                </div>
+                                <div>
+                                <ul className='clearfix'>
+                                
+                                <li className='each'>
+                                    <a href='/'>
+                                        <img src='https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2549234765.webp' />
+                                    </a>
+                                    <a className='title' href='/'>波西米亚狂想</a>
+                                    <span className='rate'>
+                                        <Rate style={{ fontSize: 10, color: "#FFAC2C" }} disabled  defaultValue={3} /> 8.3                                        
+                                    </span>
+                                    <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
+                                </li>
+                                <li className='each'>
+                                    <a href='/'>
+                                        <img src='https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2549234765.webp' />
+                                    </a>
+                                    <a className='title' href='/'>波西米亚狂想</a>
+                                    <span className='rate'>
+                                        <Rate style={{ fontSize: 10, color: "#FFAC2C" }} disabled  defaultValue={3} /> 8.3                                        
+                                    </span>
+                                    <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
+                                </li>
+                                <li className='each'>
+                                    <a href='/'>
+                                        <img src='https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2549234765.webp' />
+                                    </a>
+                                    <a className='title' href='/'>波西米亚狂想</a>
+                                    <span className='rate'>
+                                        <Rate style={{ fontSize: 10, color: "#FFAC2C" }} disabled  defaultValue={3} /> 8.3                                        
+                                    </span>
+                                    <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
+                                </li>
+                                <li className='each'>
+                                    <a href='/'>
+                                        <img src='https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2549234765.webp' />
+                                    </a>
+                                    <a className='title' href='/'>波西米亚狂想</a>
+                                    <span className='rate'>
+                                        <Rate style={{ fontSize: 10, color: "#FFAC2C" }} disabled  defaultValue={3} /> 8.3                                        
+                                    </span>
+                                    <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
+                                </li>
+                                <li className='each'>
+                                    <a href='/'>
+                                        <img src='https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2549234765.webp' />
+                                    </a>
+                                    <a className='title' href='/'>波西米亚狂想</a>
+                                    <span className='rate'>
+                                        <Rate style={{ fontSize: 10, color: "#FFAC2C" }} disabled  defaultValue={3} /> 8.3                                        
+                                    </span>
+                                    <Button style={{ backgroundColor: "#258DCC", color: "white", fontSize: '10px', padding: '2px 15px 2px 15px' }}>选座购票</Button>
+                                </li>
+                            </ul>
+                                </div>
+                            </Slider>
                         </div>
                     </div>
                     <div className='aside'></div>
